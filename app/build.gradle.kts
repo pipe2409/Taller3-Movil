f9eab36114810728144fe149971bf4fd72fc85d1
pipe2409 <andresbe1205@gmail.com>
2026-05-16 17:55:18 -0500

    Solucionar conflictos de librerias y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    // Navigation (Este es el que causaba problemas)
-    implementation(libs.navigation.compose)
+    // Navigation (Nombre corregido: libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
 
     // Firebase
     implementation(platform(libs.firebase.bom))
